a80da4e194f11f3db3b2cf5537710e4d506fa5b1
rltran-codex <rtran9@montgomerycollege.edu>
2024-12-03 19:56:39 -0500

    cleaned up regex and algorithm. it go brr now

--- puzzles/day03/main.go
+++ puzzles/day03/main.go
@@ -14,66 +14,42 @@ func main() {
 	fmt.Printf("%d\n", Part2(in))
 }
 
+func product(v1 string, v2 string) int {
+	x, _ := strconv.Atoi(v1)
+	y, _ := strconv.Atoi(v2)
+	return (x * y)
+}
+
 func Part1(input string) int {
 	r := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	result := 0
 	match := r.FindAllStringSubmatch(input, -1)
 
 	for _, v := range match {
-		x, _ := strconv.Atoi(v[1])
-		y, _ := strconv.Atoi(v[2])
-		result += (x * y)
+		result += product(v[1], v[2])
 	}
 
 	return result
 }
 
 func Part2(input string) int {
-	r1 := regexp.MustCompile(`do\(\)`)
-	r2 := regexp.MustCompile(`don't\(\)`)
-	r3 := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	r := regexp.MustCompile(`mul\((\d+),(\d+)\)|don't\(\)|do\(\)`)
 	result := 0
+	matches := r.FindAllStringSubmatch(input, -1)
+	calc := true
 
-	cmd := mapDoAndDont(r1.FindAllStringIndex(input, -1), r2.FindAllStringIndex(input, -1))
-	n1 := r3.FindAllStringSubmatchIndex(input, -1)
-	n := r3.FindAllStringSubmatch(input, -1)
-
-	calculate := true
-	// anything before the first "dont", multiply
-	// anything between "dont" - "do", do not multiply
-
-	// current multiplier index
-	midx := 0
-	sidx := len(n)
-	for i := range input {
-		// stop if no more product operands
-		if midx == sidx {
-			break
-		}
-		if c, ok := cmd[i]; ok {
-			calculate = c // update command
-		}
-
-		if i == n1[midx][0] {
-			if calculate {
-				x, _ := strconv.Atoi(n[midx][1])
-				y, _ := strconv.Atoi(n[midx][2])
-				result += (x * y)
+	for _, v := range matches {
+		switch v[0] {
+		case "don't()":
+			calc = false
+		case "do()":
+			calc = true
+		default:
+			if calc {
+				result += product(v[1], v[2])
 			}
-			midx += 1
 		}
 	}
+
 	return result
-}
-
-func mapDoAndDont(do [][]int, dont [][]int) map[int]bool {
-	m := make(map[int]bool)
-	for _, v := range do {
-		m[v[0]] = true
-	}
-	for _, v := range dont {
-		m[v[0]] = false
-	}
-
-	return m
 }

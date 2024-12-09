b43be31fd64dab20f363e3c0a0d3b8b49c8fe3fc
rltran-codex <rtran9@montgomerycollege.edu>
2024-12-09 08:57:47 -0500

    update template for faster code generation

--- template/main.go
+++ template/main.go
@@ -1,9 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	// main area to display puzzle answers
+	fmt.Printf("Part 1: %d\n", Part1())
+	fmt.Printf("Part 2: %d\n", Part2())
 }
 
 func ParsePuzzleInput() {
 	// function to parse the puzzle input from file
+}
+
+func Part1() int {
+	panic("Not yet implemented")
+}
+func Part2() int {
+	panic("Not yet implemented")
 }

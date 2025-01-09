228389f59142d6dfca7801f86b008935a5a73b90
rltran-codex <rtran9@montgomerycollege.edu>
2025-01-09 01:17:45 -0500

    completed day 6 part 2

--- puzzles/day06/main.go
+++ puzzles/day06/main.go
@@ -24,8 +24,8 @@ var DefaultDir Direction = UP
 type Guard struct {
 	CurrentPosition  []int
 	CurrentDirection Direction
-	Rotation         []Direction         // the rotation cycle (queue)
-	Visited          map[string]PathNode // map of the distinct tiles the guard visited linked to  recorded direction they were facing
+	Rotation         []Direction     // the rotation cycle (queue)
+	Visited          map[string]bool // map of the distinct tiles the guard visited linked to recorded direction they were facing
 	PatrolArea       *[][]string
 	Path             *PathNode
 }
@@ -37,40 +37,18 @@ type PathNode struct {
 	Next      *PathNode
 }
 
-func (g *Guard) StepsTaken() int {
-	return len(g.Visited)
+func (g *Guard) DistinctLocations() int {
+	results := make(map[string]struct{})
+	for curr := g.Path; curr != nil; {
+		key := utils.ParseKey(curr.X, curr.Y)
+		results[key] = struct{}{}
+		curr = curr.Next
+	}
+
+	return len(results)
 }
 
-func (g *Guard) PatrolAreaWithRotation(steps *int) {
-	// base case 1: if next step is out of bounds, guard is done patrolling
-	x, y := g.CurrentPosition[0], g.CurrentPosition[1]
-	// increment distinct steps
-	if (*g.PatrolArea)[x][y] == "." || (*g.PatrolArea)[x][y] == "^" {
-		*steps++
-	}
-
-	// mark it as stepped through
-	(*g.PatrolArea)[x][y] = "X"
-	if x-1 < 0 {
-		return
-	}
-
-	// if the next step is a #, rotate map counter clockwise to simulate moving right
-	if (*g.PatrolArea)[x-1][y] == "#" {
-		*g.PatrolArea = utils.Rotate2DSlice(*g.PatrolArea, utils.ANTICLOCK)
-
-		// update curr position since map was transposed
-		n := len(*g.PatrolArea)
-		nx, ny := n-1-y, x
-		x, y = nx, ny
-	}
-
-	g.CurrentPosition[0] = x - 1
-	g.CurrentPosition[1] = y
-	g.PatrolAreaWithRotation(steps)
-}
-
-func (g *Guard) GetPatrolPath() {
+func (g *Guard) GetPatrolPath(detectCycle bool) bool {
 	maxHeight := len(*g.PatrolArea)
 	maxWidth := len((*g.PatrolArea)[0])
 
@@ -86,21 +64,24 @@ func (g *Guard) GetPatrolPath() {
 		x, y := currNode.X, currNode.Y
 		nx, ny := calcNextStep(x, y, g.CurrentDirection)
 
-		// add this tile to visited
-		idx := parseIndices(x, y)
-		_, ok := g.Visited[idx]
-		if !ok {
-			g.Visited[idx] = *currNode
-		}
-
 		// guard's next step is out of bounds, loop done
 		if nx < 0 || nx >= maxHeight || ny < 0 || ny >= maxWidth {
-			return
+			return false
 		}
 
 		if (*g.PatrolArea)[nx][ny] == "#" {
+			// turn guard
 			g.CurrentDirection = utils.PopAndRequeue(&g.Rotation)
-			nx, ny = calcNextStep(x, y, g.CurrentDirection)
+			currNode.FacingDir = g.CurrentDirection
+			continue
+		}
+
+		if detectCycle {
+			key := parseIndices(x, y, g.CurrentDirection)
+			if g.Visited[key] {
+				return true
+			}
+			g.Visited[key] = true
 		}
 
 		nextNode := PathNode{
@@ -114,15 +95,10 @@ func (g *Guard) GetPatrolPath() {
 	}
 }
 
-func parseIndices(x int, y int) string {
+func parseIndices(x int, y int, dir Direction) string {
 	xstr := strconv.Itoa(x)
 	ystr := strconv.Itoa(y)
-	return strings.Join([]string{xstr, ystr}, ";")
-}
-
-// note: need to handle when reaching the head and the next step is not an already visited tile
-func (g *Guard) DetectCycle() bool {
-	panic("TODO")
+	return strings.Join([]string{xstr, ystr, string(dir)}, ";")
 }
 
 func calcNextStep(x int, y int, currDir Direction) (int, int) {
@@ -147,22 +123,24 @@ func calcNextStep(x int, y int, currDir Direction) (int, int) {
 
 func main() {
 	// main area to display puzzle answers
-	data1, start1 := ParsePuzzleInput()
-	data2, start2 := ParsePuzzleInput()
+	data1, start1 := ParsePuzzleInput(false)
+	data2, start2 := ParsePuzzleInput(false)
 
 	fmt.Printf("Part 1: %d\n", Part1(data1, start1))
 	fmt.Printf("Part 2: %d\n", Part2(data2, start2))
 }
 
 func Part1(data [][]string, start []int) int {
-	results := 0
 	guard := Guard{
-		PatrolArea:      &data,
-		CurrentPosition: start,
+		CurrentPosition:  []int{start[0], start[1]},
+		CurrentDirection: DefaultDir,
+		Rotation:         DefaultRot,
+		PatrolArea:       &data,
 	}
-	// patrolAreaWithRotation(&data, start, &results)
-	guard.PatrolAreaWithRotation(&results)
-	return results
+
+	// conduct part 1, get the patrol path
+	guard.GetPatrolPath(false)
+	return guard.DistinctLocations()
 }
 
 func Part2(data [][]string, start []int) int {
@@ -170,17 +148,44 @@ func Part2(data [][]string, start []int) int {
 		CurrentPosition:  []int{start[0], start[1]},
 		CurrentDirection: DefaultDir,
 		Rotation:         DefaultRot,
-		Visited:          make(map[string]PathNode),
 		PatrolArea:       &data,
 	}
 
-	guard.GetPatrolPath()
+	// conduct part 1, get the patrol path
+	guard.GetPatrolPath(false)
+	maxHeight := len(data)
+	maxWidth := len(data[0])
+	validObstacles := make(map[string]bool)
+	for curr := guard.Path; curr != nil; curr = curr.Next {
+		// create a new guard and place obstacles in front of current node
+		ng := Guard{
+			CurrentPosition:  []int{start[0], start[1]},
+			CurrentDirection: DefaultDir,
+			Rotation:         DefaultRot,
+			PatrolArea:       deepCopy2DArray(data),
+			Visited:          make(map[string]bool),
+		}
 
-	return 0
+		nx, ny := calcNextStep(curr.X, curr.Y, curr.FacingDir)
+		key := utils.ParseKey(nx, ny)
+		if validObstacles[key] {
+			continue
+		}
+
+		// if next step is in bounds, place obstacle in front of it
+		if nx < 0 || nx >= maxHeight || ny < 0 || ny >= maxWidth {
+			continue
+		}
+		(*ng.PatrolArea)[nx][ny] = "#"
+		if ng.GetPatrolPath(true) {
+			validObstacles[key] = true
+		}
+	}
+	return len(validObstacles)
 }
 
-func ParsePuzzleInput() ([][]string, []int) {
-	file := utils.GetPuzzleInput("day6.txt", true)
+func ParsePuzzleInput(sample bool) ([][]string, []int) {
+	file := utils.GetPuzzleInput("day6.txt", sample)
 	defer file.Close()
 
 	scn := bufio.NewScanner(file)
--- puzzles/day06/main_test.go
+++ puzzles/day06/main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/rltran-codex/aoc-2024-go/utils"
 )
 
 func BenchmarkPart1(b *testing.B) {
 	// set up dataset (aka puzzle data)
-	data, start := ParsePuzzleInput()
+	data, start := ParsePuzzleInput(false)
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -17,11 +20,95 @@ func BenchmarkPart1(b *testing.B) {
 
 func BenchmarkPart2(b *testing.B) {
 	// set up dataset (aka puzzle data)
-	data, start := ParsePuzzleInput()
+	data, start := ParsePuzzleInput(false)
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		// code to benchmark
 		Part2(data, start)
+	}
+}
+
+func TestSamplePart1(t *testing.T) {
+	area, start := ParsePuzzleInput(true)
+	guard := Guard{
+		CurrentPosition:  []int{start[0], start[1]},
+		CurrentDirection: DefaultDir,
+		Rotation:         DefaultRot,
+		PatrolArea:       &area,
+	}
+
+	// conduct part 1, get the patrol path
+	guard.GetPatrolPath(false)
+
+	// check if path is correct
+	expectedMap := utils.Get2DPuzzleInput("day6_path.txt", true)
+	for curr := guard.Path; curr != nil; {
+		area[curr.X][curr.Y] = "X"
+		curr = curr.Next
+	}
+
+	for r := 0; r < len(area); r++ {
+		for c := 0; c < len(area[0]); c++ {
+			if area[r][c] != expectedMap[r][c] {
+				t.Errorf("Invalid path point found at [%d, %d]", r, c)
+			}
+		}
+	}
+
+	expected := 41
+	actual := guard.DistinctLocations()
+	if expected != actual {
+		t.Error("Failed sample, did not get 41 distinct postions.")
+	}
+}
+
+func TestSampleDetection(t *testing.T) {
+	area, start := ParsePuzzleInput(true)
+	// based on the obstacles from the sample
+	expected := map[string]bool{
+		"6;3": true,
+		"7;6": true,
+		"7;7": true,
+		"8;1": true,
+		"8;3": true,
+		"9;7": true,
+	}
+	actual := make(map[string]bool)
+
+	for o := range expected {
+		coor := strings.Split(o, ";")
+		x, y := utils.Atoi(coor[0]), utils.Atoi(coor[1])
+		g := Guard{
+			CurrentPosition:  []int{start[0], start[1]},
+			CurrentDirection: DefaultDir,
+			Rotation:         DefaultRot,
+			PatrolArea:       deepCopy2DArray(area),
+			Visited:          make(map[string]bool),
+		}
+
+		(*g.PatrolArea)[x][y] = "#"
+		if g.GetPatrolPath(true) {
+			actual[o] = true
+		}
+	}
+
+	if len(actual) != len(expected) {
+		t.Errorf("Length of actual %d, expected %d", len(actual), len(expected))
+	}
+
+	for i := range actual {
+		if !expected[i] {
+			t.Errorf("Unexpected key found in actual: %s", i)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	area, start := ParsePuzzleInput(true)
+	expected := 6
+	actual := Part2(area, start)
+	if expected != actual {
+		t.Errorf("Expected %d, but actual %d", expected, actual)
 	}
 }
